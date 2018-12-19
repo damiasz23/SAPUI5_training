16a79a6d5aeee05541b403e42b085e40014ee85c
d <g>
2018-12-19 17:28:25 +0100

    Enter implementation

--- UI5_test/WebContent/ui5_test/UI5_test.controller.js
+++ UI5_test/WebContent/ui5_test/UI5_test.controller.js
@@ -20,35 +20,61 @@ sap.ui.controller("ui5_test.UI5_test", {
 //
 //	},
 	
-	onShowText : function(oEvent) {
+	onShowText : function() {
 		
-		var oDunk = this.getView().byId("dunk");
-		var oPass = this.getView().byId("pass");
-		var oCarusel = this.getView().byId("test");
-		var activePage = oCarusel.getActivePage();
+		confirm("What is the name of player on picture?");
 		
-		if(oDunk.sId === activePage){
-			alert("MJ leci jak ptak");
-		}else if(oPass.sId === activePage){
-			alert("MJ dyrygent");//		
-			
-		}
+//		var oDunk = this.getView().byId("dunk");
+//		var oPass = this.getView().byId("pass");
+//		var oCarusel = this.getView().byId("test");
+//		var activePage = oCarusel.getActivePage();
+//		
+//		if(oDunk.sId === activePage){
+//			alert("MJ leci jak ptak");
+//		}else if(oPass.sId === activePage){
+//			alert("MJ dyrygent");//		
+//			
+//		}
 	},
 	
 	onPageChanged : function(oEvent) {
 		
-		var oDunk = this.getView().byId("dunk");
-		var oPass = this.getView().byId("pass");
-		var activePage = oEvent.getSource().getActivePage();
+		var carusel = oEvent.getSource().getActivePage();
 		
 		
-		if(oDunk.sId === activePage ){
-			alert("MJ leci jak ptak");
-		}else if(oPass.sId === activePage){
-			alert("MJ dyrygent");
-			
-		}
 	},
+	
+	checkAnswer : function() {
+			
+		var oCarusel = this.getView().byId("test");
+		var activePage = oCarusel.getActivePage();
+		var oPlayer = this.getView().byId(activePage);
+		var oPlayerNameFromId = oPlayer.sId;
+		var splitName = oPlayerNameFromId.split('--');
+		var answer = this.getView().byId("answer").getValue().toLowerCase();
+		
+		if(answer === splitName[1]){
+			alert("Yes sir!!");
+		}
+		else {
+			alert("Nop");
+		}
+		
+		this.getView().byId("answer").setValue("");
+		
+	},
+	
+	onAfterRendering: function() {
+        var self = this;
+        jQuery("input").on("keydown",
+          function(evt) {
+            if (evt.keyCode == 13) {
+            self.checkAnswer();
+            }
+          });
+ },
+	
+
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
@@ -67,4 +93,3 @@ sap.ui.controller("ui5_test.UI5_test", {
 //	}
 
 });
-
